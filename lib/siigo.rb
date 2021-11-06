74fd6eb3375fa362c324c25b832f89e5e192f3cb
Andrés Carvajal <andres.carvajal@prodalca.com.co>
2021-11-06 13:36:59 -0500

    Add: classes to fetch credit-notes, journals, users and vouchers

--- lib/siigo.rb
+++ lib/siigo.rb
@@ -6,11 +6,19 @@ class Siigo
 end
 
 require 'siigo/auth'
+
+# List of "catalogos"
+
 require 'siigo/account_groups'
 require 'siigo/taxes'
 require 'siigo/cost_centers'
+require 'siigo/users'
+
 require 'siigo/customers'
 require 'siigo/products'
 require 'siigo/invoices'
+require 'siigo/credit_notes'
+require 'siigo/vouchers'
+require 'siigo/journals'
 
 require 'siigo/tools'
---
+++ lib/siigo/vouchers.rb
@@ -0,0 +1,96 @@
+class Siigo::Vouchers
+
+    def fetch(token, output_path)
+        resource_tag = "vouchers"
+
+        puts "Starting to fetch " + resource_tag + " from Siigo"
+
+        tools = Siigo::Tools.new
+        resource_size = Float(tools.get_size(resource_tag, token))
+        puts "The Siigo database has " + String(Integer(resource_size)) + " " + resource_tag
+        page_size = 100
+        page_amount = (resource_size/Float(page_size)).ceil()
+        page_amount = page_amount.ceil()
+      
+        url = 'https://api.siigo.com/v1/' + resource_tag
+
+        resources = []
+        resources_all_local = []
+        diff_prom = 0
+
+        # Related to the time needed for the task
+        time_last = Time.now
+
+        for i in 1..page_amount
+            puts "Iteration " + String(i) + " of " + String(page_amount)
+            #sleep 1 # To avoid problems related to high request-rates
+            fetched = false
+            try = 1
+            while fetched != true
+                begin 
+                    response = tools.get_page(resource_tag, token, i, page_size)
+
+                    if response.count != 0 #TODO: Review if this condition is a good one
+                        resources = response
+                        fetched = true
+                        #puts "Fetched data in " + String(try) + " try"
+                        try = try + 1
+    
+                        for j in 0..(resources.count-1)
+                            resources_all_local.push(resources[j])
+                        end
+                    else 
+                        puts "Problems with the fetched results"
+                        fetched = false
+                    end
+
+                    # Related to the time needed for the task
+                    time_now = Time.now
+                    diff = time_now - time_last
+                    diff_prom = Float(diff_prom*(i-1))/Float(i) + Float(diff)/Float(i)   
+                    #diff_prom = Float(diff)/Float(i)         
+                    time_last = time_now
+                    #puts "Diff = " + String(diff)
+                    #puts "Diff prom = " + String(diff_prom)
+                    puts "Remaning time = " + String(diff_prom * (page_amount - i))
+
+                rescue => e1
+                    puts "Enter in rescue"
+                    puts e1.response
+                    fetched = false
+                    try = try + 1
+                end
+            end
+        end
+
+        puts "Writing data to the file. This could take a while..."
+        n = 0
+
+        headers = ["id", "number", "date", "type", 
+                   "customer", "items", "payment", "created_at"]
+        CSV.open(output_path, "w") do |csv|
+            csv << headers
+        
+            resources_all_local.each do |resource_local|
+                csv << [String(resource_local["id"]), 
+                        String(resource_local["number"]), 
+                        String(resource_local["date"]),
+                        String(resource_local["type"]),
+                        String(resource_local["customer"]["id"]),
+                        String(resource_local["items"]),
+                        String(resource_local["payment"]),
+                        String(resource_local["metadata"]["created"])]
+
+                n = n + 1
+                if n % 1000 == 0
+                    puts String(n) + " data has been written to the file"
+                end
+
+            end            
+        end
+
+        puts String(resources_all_local.count) + " " + resource_tag + " were written to the file ✅"
+        puts ""
+    end
+    
+end
